debf1b92acac15ea4f9ca716e7532bdb0465bf58
+ <+>
2024-01-18 22:25:24 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const { reflectURLAttribute } = require("jsdom/lib/jsdom/utils");
+
+function writeCards(names,eventName){
+const newArray=[]
+for (let index = 0; index < names.length; index++) {
+    const element = names[index];
+    newArray.push(`Thank you, ${element}, for the wonderful ${eventName} gift!`)
+}
+return newArray
+}
+
+function writeCards(newArray, eventName) {for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+    
+}
